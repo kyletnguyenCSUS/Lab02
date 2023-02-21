ccc622e1748debd7a13554f3822df4d47e349385
NicoMazzella <125933337+NicoMazzella@users.noreply.github.com>
2023-02-20 17:14:24 -0800

    Update Hello.java

--- Lab02/src/Hello.java
+++ Lab02/src/Hello.java
@@ -8,7 +8,7 @@ public class Hello {
 		System.out.println("302538119 - This line was added by Kyle Nguyen.");
 		System.out.println("219850931 - This line was added by Elijah Dela Cruz.");
 		System.out.println("302327246 - This line was added by Anna Tran.");
-		System.out.println("SacStateID - This line was added by Nicholas Mazzella.");
+		System.out.println("301527005 - This line was added by Nicholas Mazzella.");
 		System.out.println("303125277 - This line was added by Balraj Kalathil.");
 		System.out.println("SacStateID - This line was added by First name Last name.");
 		System.out.println("SacStateID - This line was added by First name Last name.");
