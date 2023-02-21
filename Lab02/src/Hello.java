2fd0a8e6169a15a19bb7175ede0c1842139e7d2c
anhqtr <123520962+anhqtr@users.noreply.github.com>
2023-02-20 16:57:34 -0800

    Update Hello.java

--- Lab02/src/Hello.java
+++ Lab02/src/Hello.java
@@ -7,7 +7,7 @@ public class Hello {
 		// Add a new line with your information
 		System.out.println("302538119 - This line was added by Kyle Nguyen.");
 		System.out.println("219850931 - This line was added by Elijah Dela Cruz.");
-		System.out.println("SacStateID - This line was added by First name Last name.");
+		System.out.println("302327246 - This line was added by Anna Tran.");
 		System.out.println("SacStateID - This line was added by First name Last name.");
 		System.out.println("SacStateID - This line was added by First name Last name.");
 		System.out.println("SacStateID - This line was added by First name Last name.");
