848530ab62b1ad15e2e78c1c33568961d2d4eaaa
ElijahDLC <74538183+ElijahDLC@users.noreply.github.com>
2023-02-20 16:54:52 -0800

    Update Hello.java

--- Lab02/src/Hello.java
+++ Lab02/src/Hello.java
@@ -6,7 +6,7 @@ public class Hello {
 		System.out.println("yang - This line was added by Dr. Yang.");
 		// Add a new line with your information
 		System.out.println("302538119 - This line was added by Kyle Nguyen.");
-		System.out.println("SacStateID - This line was added by Elijah Dela Cruz.");
+		System.out.println("219850931 - This line was added by Elijah Dela Cruz.");
 		System.out.println("SacStateID - This line was added by First name Last name.");
 		System.out.println("SacStateID - This line was added by First name Last name.");
 		System.out.println("SacStateID - This line was added by First name Last name.");
